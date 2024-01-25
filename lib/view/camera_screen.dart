0577e5ff46544f58553f1b2b64d68dbbf9ba73f1
Kazuki Aishima <kazuki.aishima.dev@gmail.com>
2024-01-25 17:01:01 +0900

    UI

--- lib/view/camera_screen.dart
+++ lib/view/camera_screen.dart
@@ -17,6 +17,8 @@ class CameraScreen extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final cameraControllerFuture = ref.watch(controllerProvider);
 
+    // iosのシャッター音を消すためのダミーcallback
+
     // コントローラーの状態を管理するためのuseStateフック
     final cameraController = useState<CameraController?>(null);
 
@@ -68,19 +70,15 @@ class CameraScreen extends HookConsumerWidget {
         if (controller.value.isInitialized)
           AspectRatio(
             aspectRatio: controller.value.aspectRatio,
-            child: Column(
-              children: [
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.8,
-                  child: CameraPreview(controller),
-                ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.2,
-                  child: Text(ref.watch(responseProvider)),
-                ),
-              ],
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height * 0.8,
+              child: CameraPreview(controller),
             ),
           ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.2,
+          child: Text(ref.watch(responseProvider)),
+        ),
       ],
     );
   }
